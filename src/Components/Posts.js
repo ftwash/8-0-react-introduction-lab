7e7c9f3a125bf666f1d703859262bebda16e6a2e
Cristobal Arias <cristobal.arias.13@gmail.com>
2021-10-29 12:44:25 -0400

    Preliminary lab, tests not passing

---
+++ src/Components/Posts.js
@@ -0,0 +1,13 @@
+import Post from "./Post";
+
+const Posts = () => {
+  return (
+    <div className="posts">
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+};
+
+export default Posts;
